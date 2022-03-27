5ffb33f30ed8110170774198c23bb10880de5af3
Alex <alex.beciana@gmail.com>
2022-03-27 13:16:03 -0400

    Added spacing and gaps to grid layout

--- src/components/Layouts/GridLayout/GridLayout.js
+++ src/components/Layouts/GridLayout/GridLayout.js
@@ -4,6 +4,10 @@ const GridWrapper = styled.section`
     display: grid;
     grid-template-columns: repeat(${props => props.col}, minmax(0, 1fr));
     align-items: stretch;
+    grid-row: auto;
+    gap: 20px;
+    margin-top: 20px;
+    margin-bottom: 20px;
 `
 
 const GridLayout = (props) => {
