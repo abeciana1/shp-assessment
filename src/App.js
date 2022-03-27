29f49cee78ff7cbc50edfcadfd7be0c4178c5fb7
Alex <alex.beciana@gmail.com>
2022-03-26 22:38:23 -0400

    Created Home page && added route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+//! import pages here
+import Home from './pages/Home'
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Switch>
+        <Route
+          path="/"
+          component={Home}
+        />
+      </Switch>
+    </Router>
   );
 }
 
