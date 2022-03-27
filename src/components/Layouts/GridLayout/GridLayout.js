7a4a07789350f17465cdc8122c5d280c585d2411
Alex <alex.beciana@gmail.com>
2022-03-27 13:05:59 -0400

    Added dynamic grid layout styling

--- src/components/Layouts/GridLayout/GridLayout.js
+++ src/components/Layouts/GridLayout/GridLayout.js
@@ -1,14 +1,17 @@
 import styled from 'styled-components'
 
 const GridWrapper = styled.section`
-    
+    display: grid;
+    grid-template-columns: repeat(${props => props.col}, minmax(0, 1fr));
+    align-items: stretch;
 `
 
-const GridLayout = ({ children }) => {
+const GridLayout = (props) => {
+    console.log(props)
 
     return (
-        <GridWrapper>
-            {children}
+        <GridWrapper col={props.col}>
+            {props.children}
         </GridWrapper>
     )
 }
